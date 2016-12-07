2e033a7682e82c8aadb780f0337e04354bb3440e
Meghana_Valluri <mvalluri@andrew.cmu.edu>
2016-12-07 09:11:17 -0500

    added model, controller, and route for getting surveys created by a given user

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   get 'users/:id/info', :to => 'users#profile_info'
   get 'users/:id/takeableSurveys', :to => 'users#filtered_surveys'
+  get 'users/:id/createdSurveys', :to => 'users#created_surveys'
 
   resources :choices, except: [:new, :edit]
   resources :questions, except: [:new, :edit]
