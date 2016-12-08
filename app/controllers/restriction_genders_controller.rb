34f0e45dc7dcbadfcef84ed7943f374b26f6dd2e
Annette Chen <a.chen1251@yahoo.com>
2016-12-08 16:43:21 -0500

    added restriciton ethnicities and genders

---
+++ app/controllers/restriction_genders_controller.rb
@@ -0,0 +1,36 @@
+class RestrictionGenderssController < ActionController::API
+	before_action :set_restriction_gender, only: [:show, :update, :destroy]
+
+	def index
+	  @restriction_genders	= RestrictionGenders.all
+
+	  render json: @restriction_genders
+	end
+
+	def show
+	  render json: @restriction_gender
+	end
+
+	def create
+		@restriction_gender = RestrictionGender.create(restriction_gender_params)
+		if @restriction_gender.save!
+			render json: @restriction_gender
+		end
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+
+	private
+		def set_restriction_gender
+	      @restriction_gender = RestrictionGender.find(params[:id])
+	    end
+
+	    def restriction_gender_params
+	      params.require(:restriction_gender).permit(:restriction_id, :gender_id)
+	    end
+end
